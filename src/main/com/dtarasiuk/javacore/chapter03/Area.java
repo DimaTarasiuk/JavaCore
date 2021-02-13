9117479ea2f3077b974dae18c9a3eb557fbd7bcd
dtarasiuk <mr.tarasiuk@gmil.com>
2021-02-13 02:01:40 +0200

    chapter03

---
+++ src/main/com/dtarasiuk/javacore/chapter03/Area.java
@@ -0,0 +1,13 @@
+package main.com.dtarasiuk.javacore.chapter03;
+
+public class Area {
+
+    public static void main(String args[]) {
+        double pi, r, а;
+        r = 10.8;
+        pi = 3.1416;
+        а = pi * r * r;
+
+        System.out.println("Плoщaдь круга равна" + а);
+    }
+}
